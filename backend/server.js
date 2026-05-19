99e145ea6c3bf8cca9cb928138555f717639600a
Sebavaldes <seba.valdes.es@gmail.com>
2026-05-18 20:22:28 -0400

    Conectar backend con MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 // Permite comunicación con frontend
 app.use(cors());
+app.use(express.json());
+
+// conexión MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error) => console.log("Error al conectar", error));
 
 // Ruta de prueba
 app.get("/", (req, res) => {
@@ -12,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Puerto
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
