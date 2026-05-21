5e88cdba5a1610d89f856145f2af0d714bfab048
Sebavaldes <seba.valdes.es@gmail.com>
2026-05-20 23:04:43 -0400

    Conectar frontend con backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
-function app(){
+import { useEffect, useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const getTasks = async () => {
+    const response = await fetch("http://localhost:3000/tasks");
+    const data = await response.json();
+    setTasks(data);
+  };
+
+  const createTask = async (e) => {
+    e.preventDefault();
+
+    if (title.trim() === "") return;
+
+    await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    setTitle("");
+    getTasks();
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <div>
       <h1>Gestor Personal</h1>
-      <p>Mi primera app full stack</p>
+
+      <form onSubmit={createTask}>
+        <input
+          type="text"
+          placeholder="Escribe una tarea"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button type="submit">Agregar</button>
+      </form>
+
+      <h2>Mis tareas</h2>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   );
-} 
+}
 
-export default app;
+export default App;
